73c29e2b4fd07a13f69c37ebf2ae97e498e03765
threen <sungate001@qq.com>
2013-02-19 15:57:37 +0800

    Demo.java V3

--- src/git/demo/Demo.java
+++ src/git/demo/Demo.java
@@ -8,7 +8,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Fire fox OK!");
+		System.out.println("Firefox is OK!");
 
 	}
 
